3b1b40ece60c51fd4b475a8a6eced7b39b3dec48
Jeff Knowles <JeffKnowles@jeffs-mbp.attlocal.net>
2017-02-22 23:09:45 -0500

    Homework Submission

--- assets/javascript/Timer.js
+++ assets/javascript/Timer.js
@@ -1,45 +1,96 @@
-//  Simple Timer Solution
+//Javascript code for a simple countdown timer which alerts "Time's Up"
 
-//  Step 1:
-//  Use the following link inside the Audio function below:
-//  https://p.scdn.co/mp3-preview/ed5a443bc86176135ebca8a114f66f4d814d4c90
-var audio = new Audio("raven.mp3");
+var count=120;
 
-//  Step 2:
-//  after 5 seconds, execute the fiveSeconds function
-//  after 10 seconds, execute the tenSeconds function
-//  after 15 seconds, execute the timeUp function
+var counter=setInterval(timer, 1000); 
 
-setTimeout(fiveSeconds, 1000 * 5);
-setTimeout(tenSeconds, 1000 * 10);
-setTimeout(timeUp, 1000 * 15);
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     alert("Times Up!");
+     reset();
+  }
 
-//  Step 3:
-//  Fill in the blanks to these functions.
-function fiveSeconds() {
+ document.getElementById("timer").innerHTML=count + " seconds";
 
-  // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
-  // console log 10 seconds left
-  $("#time-left").append("<h2>About 10 Seconds Left!</h2>");
-  console.log("10 seconds left");
 }
 
-function tenSeconds() {
+// Global variables used to progress through questions, track answers, and end the game when time expires
 
-  // in the element with an id of time-left add an h2 saying About 5 Seconds Left!
-  // console log 5 seconds left
-  $("#time-left").append("<h2>About 5 Seconds Left!</h2>");
-  console.log("5 seconds left");
+var questionNumber = 0;
+var rightAnswers = 0;
+var timeUp = false;
+
+
+//Array of objects which contains a list of questions, answer choices, and the index position of the correct answer
+
+var questions = [{
+    question: "Name the seventh planet from the sun:",
+    choices: ["Pluto", "Mars", "Jupiter", "Uranus"],
+    answer: 2
+}, {
+    question: "Name the world's largest ocean:",
+    choices: ["Atlantic", "Indian", "Pacific", "Arctic"],
+    answer: 2
+}, {
+    question: "Which chess piece can only move diagonally:",
+    choices: ["Rook", "Pawn", "Bishop", "King"],
+    answer: 2
+}, {
+    question: "How many valves does a trumpet have:",
+    choices: ["Three", "Two", "One", "None"],
+    answer: 0
+}, {
+    question: "What flavour is Cointreau:",
+    choices: ["Watermelon", "Orange", "Bananna", "Coconut"],
+    answer: 1
+}, {
+    question: "How many degrees are found in a circle:",
+    choices: ["45", "90", "180", "360"],
+    answer: 3
+}, {
+    question: "The beaver is the national emblem of which country:",
+    choices: ["Sweden", "Canada", "Austria", "Brazil"],
+    answer: 1
+}, {
+    question: "How many players are there in a baseball team:",
+    choices: ["8", "12", "10", "9"],
+    answer: 3
+}, {
+    question: "Hg is the chemical symbol of which element:",
+    choices: ["Mercury", "Helium", "Mangnesium", "Calcium"],
+    answer: 0
+}, {
+    question: "The Statue of Liberty was given to the US by which country:",
+    choices: ["Germany", "Paris", "Great Britain", "Japan"],
+    answer: 1
+}];
+
+  $("#a1").html(questions[0].choices.[0]);
+  $("#a2").html(questions[0].choices.[1]);
+  $("#a3").html(questions[0].choices.[2]);
+  $("#a4").html(questions[0].choices.[3]);
+
+
+
+// $(document).ready(function () {
+
+ 
+
+
+function reset() {
+    currentQuestion = 0;
+    correctAnswers = 0;
 }
 
-function timeUp() {
 
-  // in the element with an id of time-left add an h2 saying Time's Up!
-  // console log done
-  console.log("done");
-  $("#time-left").append("<h2>Time's Up!</h2>");
-  console.log("time is up");
 
-  //  The following line will play the audio file we linked to above:
-  audio.play();
-}
+
+
+
+
+
+    
